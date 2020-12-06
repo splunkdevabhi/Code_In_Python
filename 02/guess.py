6f49987cbed250994c371f977d6e796919bdd294
splunkdevabhi <shukla.abhishek2011@live.com>
2020-12-06 16:44:22 +0530

    first commit

---
+++ 02/guess.py
@@ -0,0 +1,30 @@
+import random
+
+number = random.randint(1,25)
+
+#number_of_guesses = 0 
+
+for i in range(5):
+# while number_of_guesses 5:
+    print('Guess a no between 1 and 25:')
+    guess = input()
+    guess = int(guess)
+
+    #number_of_guesses = number_of_guesses +1
+
+    if guess < number:
+        print('Your guess is too low')
+
+    if guess > number:
+        print('Your guess is too high')
+
+    if guess == number:
+        break
+
+if guess == number:
+    print('You guesses the number right')
+
+else:
+    print('You did not guess the number. The number was' + str(number))
+
+
---
+++ 02/test.py
@@ -0,0 +1,2 @@
+sammy = {'name':'abhi','age':'31'}
+print(sammy)
